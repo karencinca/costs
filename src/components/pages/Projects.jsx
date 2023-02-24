48f176e8f1123a87ae0aab00fe52289432e9ee92
Karen <karencinca@hotmail.com>
2023-02-24 19:17:50 -0300

    projects details

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -4,6 +4,7 @@ import { useLocation } from "react-router-dom"
 
 import Message from "../layout/Message"
 import Container from '../layout/Container'
+import Loading from '../layout/Loading'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
 import { useEffect, useState } from 'react'
@@ -11,6 +12,8 @@ import { useEffect, useState } from 'react'
 function Projects() {
 
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -27,8 +30,23 @@ function Projects() {
         }).then((resp) => resp.json())
         .then((data) => {
             setProjects(data)
+            setRemoveLoading(true)
         }).catch(err => console.log(err))
     }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(resp => resp.json())
+        .then(() => {
+            setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch(err => console.log(err))
+    }
 
     return (
         <div className={styles.project_container}>
@@ -37,6 +55,7 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto" />
             </div>   
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => (
@@ -46,8 +65,13 @@ function Projects() {
                         budget={project.budget}
                         category={project?.category?.name}
                         key={project.id}
+                        handleRemove={removeProject}
                         />
                 ))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
